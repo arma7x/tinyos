e49d0488c4a445f975feda180708ad3dc94f704f
Ahmad Malik <arma7x@live.com>
2022-09-17 02:36:42 +0800

    add AHT10 & BH1750 sensor

--- TINYOS.ino
+++ TINYOS.ino
@@ -6,6 +6,48 @@
 #include "register_module.h"
 #include "notification_bar.h"
 
+#include <AHTxx.h>  
+AHTxx aht10(AHTXX_ADDRESS_X38, AHT1x_SENSOR);
+
+#include <Wire.h>
+#include <BH1750.h>
+BH1750 lightMeter;
+
+void taskaht10(void *pvParameters) {
+  float ahtValue;
+  for(;;) {
+    while (aht10.begin() != true) {
+      Serial.println(F("AHT1x not connected or fail to load calibration coefficient"));
+      vTaskDelay(5000);
+    }
+    ahtValue = aht10.readTemperature();
+    Serial.print(F("Temperature...: "));
+    if (ahtValue != AHTXX_ERROR) {
+      Serial.print(ahtValue);
+      Serial.println(F(" +-0.3C"));
+    }
+    ahtValue = aht10.readHumidity();
+    Serial.print(F("Humidity......: "));
+    if (ahtValue != AHTXX_ERROR) {
+      Serial.print(ahtValue);
+      Serial.println(F(" +-2%"));
+    }
+    vTaskDelay(30000);
+  }
+}
+
+void taskbh1750(void *pvParameters) {
+  for (;;) {
+    if (lightMeter.measurementReady()) {
+      float lux = lightMeter.readLightLevel();
+      Serial.print(F("\nLight: "));
+      Serial.print(lux);
+      Serial.println(F(" lx"));
+    }
+    vTaskDelay(30000);
+  }
+}
+
 void setup()
 {
   Serial.begin(115200);
@@ -27,9 +69,14 @@ void setup()
 
   ModuleSwitcher(GetModuleHomescreen());
   GetActiveModule().Init(0);
-  
+
+  watch();  
   Serial.println(F("RUNNING TINYOS"));
-  watch();
+  
+  Wire.begin();
+  lightMeter.begin();
+  xTaskCreatePinnedToCore(taskaht10, "taskaht10", 1024, NULL, 3, NULL, ARDUINO_RUNNING_CORE);
+  xTaskCreatePinnedToCore(taskbh1750, "taskbh1750", 1024, NULL, 3, NULL, ARDUINO_RUNNING_CORE);
 }
 
 void loop()
